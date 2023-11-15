cf2ff8b20bb97e2ca24157888609767c6f441ea5
brew-will <wstenner@brewdigital.uk>
2023-11-15 12:26:53 +0000

    pr script

---
+++ open-pr.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+base_branch=$(git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+
+title=""
+template="new-module.md"
+org="brew-will"
+pro=$(git rev-parse --show-toplevel)
+
+if [ "$current_branch" == $base_branch ]; then
+    echo "You can not run this script from the main branch"
+    exit 1
+fi
+
+git ls-remote --exit-code origin "$current_branch"
+
+if [ $? -ne 0 ]; then
+    echo "There is no remote version of the branch, please commit any changes and push."
+    exit 1
+fi
+
+if [ -n "$1" ]; then
+    title=$(echo "$1" | sed 's/ /+/g')
+else
+    echo "Please provide a title as the first argument" 
+    exit 1
+fi
+
+if [ -n "$2" ]; then
+    template=$2
+fi
+
+echo "https://github.com/$org/$pro/compare/$base_branch...$current_branch?quick_pull=1&title=$title&template=$template"
